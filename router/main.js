fd0503d0cdb99d588b372402b3e7a41c45d1b73e
wangsoonphil <74697986+wangsoonphil@users.noreply.github.com>
2020-11-20 10:46:22 +0900

    Add files via upload

---
+++ router/main.js
@@ -0,0 +1,16 @@
+module.exports = function(app)
+{
+     app.get('/',function(req,res){
+        res.render('index.html')
+     });
+     app.get('/about',function(req,res){
+        res.render('about.html');
+    });
+	
+	app.get('/login',function(req,res){
+        res.render('login.html')
+     });
+	 app.get('/cover',function(req,res){
+        res.render('cover.html')
+     });
+}
